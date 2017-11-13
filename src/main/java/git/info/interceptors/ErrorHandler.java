e2aa733a69dfbc0857a52f7b69b2ae54257ccc85
PolskiKiel <PolskiKiel>
2017-11-13 22:15:03 +0100

    app ready

--- src/main/java/git/info/interceptors/ErrorHandler.java
+++ src/main/java/git/info/interceptors/ErrorHandler.java
@@ -1,11 +1,13 @@
 package git.info.interceptors;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class ErrorHandler implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
