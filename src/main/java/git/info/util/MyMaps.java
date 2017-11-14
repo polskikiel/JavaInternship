4ea92724a559ad07bdddfb5882729bba3735d469
PolskiKiel <PolskiKiel>
2017-11-14 23:40:22 +0100

    sorting stats

--- src/main/java/git/info/util/MyMaps.java
+++ src/main/java/git/info/util/MyMaps.java
@@ -1,7 +1,6 @@
 package git.info.util;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,15 +8,13 @@ import java.util.stream.Collectors;
 public class MyMaps {
     public static Map<String, Integer> sortMapByValue(Map<String, Integer> map) {
 
-        Map<String, Integer> map1 = map.entrySet().stream().    //  iterate through map entries
+        return map.entrySet().stream().    //  iterate through map entries
                 sorted(Map.Entry.comparingByValue(Collections.reverseOrder())). //  sorting it by value descending
                 collect(Collectors.toMap(
                 Map.Entry::getKey,
                 Map.Entry::getValue,   //  then collect it to LinkedHashMap with same key and value
-                (integer, integer2) -> integer,     // merge func
+                (integer, integer2) -> integer,
                 LinkedHashMap::new      // have to be Linked to sort ( HashMap didn't )
         ));
-
-        return map1;
     }
 }
