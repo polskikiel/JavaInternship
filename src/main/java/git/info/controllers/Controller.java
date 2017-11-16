0137fd8a9d0e40e0702eef69809329f020db4bfb
PolskiKiel <PolskiKiel>
2017-11-16 12:42:57 +0100

    access token in cookie

--- src/main/java/git/info/controllers/Controller.java
+++ src/main/java/git/info/controllers/Controller.java
@@ -4,10 +4,14 @@ import git.info.services.GitServices;
 import git.info.services.MySessionServices;
 import lombok.AllArgsConstructor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @org.springframework.stereotype.Controller
 @AllArgsConstructor
@@ -17,9 +21,9 @@ public class Controller {
     MySessionServices sessionServices;
 
     @GetMapping({"", "/"})
-    public String getAuth() {
+    public String getAuth(@CookieValue("gittoken") String token) {
 
-        if(sessionServices.getToken() != null)
+        if(sessionServices.getToken() != null || token != null)      // if we get token already we can go straight to the user data
             return "redirect:/git2";
 
         //  connect with github api
@@ -30,20 +34,34 @@ public class Controller {
 
     @RequestMapping("/git")     // GIT CALLBACK URL
     public String getToken(@RequestParam("code") String code,
-                      @RequestParam("state") String state) {
+                           @RequestParam("state") String state,
+                           HttpServletResponse response) {
 
         if (!sessionServices.checkState(state) || code == null) {
             return "redirect:/errors?nr=401";
             // "If the states don't match, the request was created by a third party and the process should be aborted."
         }
 
-        sessionServices.setToken(gitServices.getAccessToken(code));
+
+        String token = gitServices.getAccessToken(code);
+        sessionServices.setToken(token);
+
+        response.addCookie(new Cookie("gittoken", token));
 
         return "redirect:/git2";
     }
 
     @RequestMapping("/git2")
-    public String setUser() {       // data refresh
+    public String setUser(@CookieValue("gittoken") String token) {       // data refresh
+
+        if (token != null) {
+            try {
+                sessionServices.setUser(
+                        gitServices.getUserInfo(token));
+
+            } catch (Exception e) {
+            }
+        }
 
         try {
             sessionServices.setUser(            // repos, user info
@@ -59,7 +77,7 @@ public class Controller {
 
     @GetMapping("/info")
     public String info(Model model) {
-        if (!sessionServices.hasUser()) {   // authorize again
+        if (!sessionServices.hasUser()) {
             return "redirect:/errors?nr=404";
         }
         model.addAttribute("user", sessionServices.getUser());
@@ -74,6 +92,7 @@ public class Controller {
     @RequestMapping("/errors")
     public String renderErrorPage(@RequestParam("nr") Integer nr) {
 
+        System.out.println(nr);
 
         // authorize again after every error
         sessionServices.setToken(null);
